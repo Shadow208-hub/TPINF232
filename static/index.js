260bd2a8a72ea7b70892d83d0b6b6573a3b0b20e
Shadow208-hub <brandotatsa@gmail.com>
2026-04-29 19:29:43 +0100

    Update index.js

--- static/index.js
+++ static/index.js
@@ -228,7 +228,8 @@ document.addEventListener('DOMContentLoaded', () => {
             try {
                 const response = await fetch(`${API_BASE_URL}/donnees/`, {
                     method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
+                    headers: { 'Content-Type': 'application/json',
+                             'Accept': 'application/json'},
                     body: JSON.stringify(payload)
                 });
 
