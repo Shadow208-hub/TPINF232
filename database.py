61b1f4a9b2afc503c805e08af7305aeed92c6308
Shadow208-hub <brandotatsa@gmail.com>
2026-04-29 11:48:48 +0100

    Fix typos in database connection handling
    Fixed typo in 'startswith' method calls and corrected engine creation syntax.

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL","sqlite:///./app.db")  # Correction: chemin relatif correct
-if SQLALCHEMY_DATABASE_URL.startwith("postgres://"):
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://")
 
-Connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startwith("sqlite") else {}
-moteur = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=Connect_args})
+Connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
+moteur = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=Connect_args)
 Localsession = sessionmaker(autocommit=False, autoflush=False, bind=moteur)
 
 Base = declarative_base()
